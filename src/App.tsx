3d050e7b66c99718dbfbb4f5f12dd3c8d71895e2
tbmcabee <tallon.mcabee@gmail.com>
2023-06-15 10:14:53 -0400

    Nav Tab + DOM Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
-import NavTab from "./components/navTab";
+//import { useState } from "react";
+import Home from "./components/Home";
+import TestPage from "./components/TestPage";
+import { BrowserRouter as Router, Routes, Route }
+    from "react-router-dom";
 
 function App() {
-
-  let tabs = ["Home", "About", "Projects", "Experience"]
   return (
-    <div>
-      <h1 className="text-center">My Resume Website</h1>
-      <NavTab tabs={tabs}/>
-    </div>
+  <Router>
+    <Routes>
+      <Route path="/Home" element={<Home />}/>
+      <Route path="/About" element={<TestPage />}/>
+      <Route path="/Projects" element={<TestPage />}/>
+      <Route path="/Experience" element={<TestPage />}/>
+      <Route path="/" element={<Home />}/>
+    </Routes>
+  </Router>
   );
 }
 
--- src/components/NavTab.tsx
+++ src/components/NavTab.tsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
-interface NavProps {
-  tabs: string[];
-}
-
-const NavTab = ({ tabs }: NavProps) => {
+function NavTab() {
+  let tabs = ["Home", "About", "Projects", "Experience"]
   const [tabIndex, setTabIndex] = useState(-1);
 
+  const navigate = useNavigate();
+  
+  const goToPage = (pageName: string) => {
+    navigate('/' + pageName); 
+  };
+ 
   return (
   <nav className="nav nav-pills justify-content-center">
     {tabs.map((tab, index) => (
       <li className="nav-item">
-        <a className={tabIndex === index ? "nav-link active" : "nav-link"} key={tab} onClick={() => {setTabIndex(index)}} href="#">{tab}</a>
+        <a className={tabIndex === index ? "nav-link active" : "nav-link"} key={tab} onClick={() => 
+          {setTabIndex(index); goToPage(tab);}}>{tab}</a>
       </li>
     ))}
   </nav>
