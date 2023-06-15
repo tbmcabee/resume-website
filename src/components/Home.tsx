2eb3bd31ee26501d88c0d92b26fe9971929788ee
tbmcabee <tallon.mcabee@gmail.com>
2023-06-15 10:14:20 -0400

    Added NavTab functionality and DOM routing between pages/componenets

---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavTab from './NavTab'
+
+function Home() {
+
+  return (
+    <div>
+        <h1 className="text-center">My Resume Website</h1>
+        <NavTab />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/TestPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavTab from './NavTab'
+
+function TestPage() {
+    
+  return (
+    <div>
+        <h1>WELCOME TO THE TEST PAGE</h1>
+        <NavTab />
+        <p>This is the Test Page!!!</p>
+    </div>
+  )
+}
+
+export default TestPage
