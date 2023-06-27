197963dcdb48397393b33131d8d58a364a1ad58b
tbmcabee <tallon.mcabee@gmail.com>
2023-06-27 09:21:17 -0400

    Pushing experience timeline

--- src/components/Pages/Experience/Experience.tsx
+++ src/components/Pages/Experience/Experience.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import NavTab from '../../NavTab'
 import PageBlock from '../../PageBlock/PageBlock'
-import styles from './Experience.module.css'
+import './Experience.css'
+import { MDBCard, MDBCardBody, MDBContainer } from "mdb-react-ui-kit";
 
 const Experience = () => {
   return (
@@ -10,8 +11,8 @@ const Experience = () => {
         <h1 className="text-center">My Resume Website</h1>
         <NavTab />
     </div>
-    <div className={styles.alignDiv}>
-        <h5 className={styles.center}>Experience Timeline</h5>
+    <div className='align-div'>
+        <h5 className='center'>Experience Timeline</h5>
         <PageBlock header='Job Example #1'>
         Experiences I had in this job!!! Collaborates with TD Synnex Sales Reps, 
         Reseller Reps, End Users and internal IBM Employees on technical solutions featuring: IBM Power 9 and 10 servers, 
@@ -27,6 +28,90 @@ const Experience = () => {
         Reseller Reps, End Users and internal IBM Employees on technical solutions featuring: IBM Power 9 and 10 servers, 
         AIX, IBM i and Linux distributions 
         </PageBlock>
+    </div>
+    <div>
+      <MDBContainer fluid className="py-5" style={{ backgroundColor: "#F0F2F5" }}>
+        <div className="main-timeline">
+          <div className="timeline left">
+            <MDBCard>
+              <MDBCardBody className="p-4">
+                <h3>2017</h3>
+                <h5>May to April</h5>
+                <PageBlock header='Job Example #1'>
+                  Experiences I had in this job!!! Collaborates with TD Synnex Sales Reps, 
+                  Reseller Reps, End Users and internal IBM Employees on technical solutions featuring: IBM Power 9 and 10 servers, 
+                  AIX, IBM i and Linux distributions 
+                </PageBlock>
+              </MDBCardBody>
+            </MDBCard>
+          </div>
+          <div className="timeline right">
+            <MDBCard>
+              <MDBCardBody className="p-4">
+                <h3>2016</h3>
+                <p className="mb-0">
+                  Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+                  admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+                  quis iuvaret expetendis his, te elit voluptua dignissim per,
+                  habeo iusto primis ea eam.
+                </p>
+              </MDBCardBody>
+            </MDBCard>
+          </div>
+          <div className="timeline left">
+            <MDBCard>
+              <MDBCardBody className="p-4">
+                <h3>2015</h3>
+                <p className="mb-0">
+                  Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+                  admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+                  quis iuvaret expetendis his, te elit voluptua dignissim per,
+                  habeo iusto primis ea eam.
+                </p>
+              </MDBCardBody>
+            </MDBCard>
+          </div>
+          <div className="timeline right">
+            <MDBCard>
+              <MDBCardBody className="p-4">
+                <h3>2012</h3>
+                <p className="mb-0">
+                  Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+                  admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+                  quis iuvaret expetendis his, te elit voluptua dignissim per,
+                  habeo iusto primis ea eam.
+                </p>
+              </MDBCardBody>
+            </MDBCard>
+          </div>
+          <div className="timeline left">
+            <MDBCard>
+              <MDBCardBody className="p-4">
+                <h3>2011</h3>
+                <p className="mb-0">
+                  Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+                  admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+                  quis iuvaret expetendis his, te elit voluptua dignissim per,
+                  habeo iusto primis ea eam.
+                </p>
+              </MDBCardBody>
+            </MDBCard>
+          </div>
+          <div className="timeline right">
+            <MDBCard>
+              <MDBCardBody className="p-4">
+                <h3>2007</h3>
+                <p className="mb-0">
+                  Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+                  admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+                  quis iuvaret expetendis his, te elit voluptua dignissim per,
+                  habeo iusto primis ea eam.
+                </p>
+              </MDBCardBody>
+            </MDBCard>
+          </div>
+        </div>
+      </MDBContainer>
     </div>
     </>
   )
